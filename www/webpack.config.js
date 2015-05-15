8e893c27582772cc7fe028ad1e29c8bd3e8f3b67
kRITZCREEK <hegi-power@web.de>
2015-05-15 10:55:27 +0200

    macht jQuery mit webpack verfuegbar.

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,23 +1,34 @@
+import webpack from 'webpack'
+
 module.exports = {
     entry: {
-      app: ['./js/main.js']
+        app: ['./js/main.js']
     },
     output: {
-      path: __dirname,
-      publicPath: "/",
-      filename: 'bundle.js'
+        path: __dirname,
+        publicPath: "/",
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
             //{ test: /\.css$/, loader: "style!css" },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            },
+            {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
         ]
     },
     externals: {
         //don't bundle the 'react' npm package with our bundle.js
         //but get it from a global 'React' variable
-        'react': 'React'
-    }
+        //'react': 'React'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 };
